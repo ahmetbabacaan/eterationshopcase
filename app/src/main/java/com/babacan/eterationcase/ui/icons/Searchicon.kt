2f42f7a44df55a0b1b2e4f95443f0caeed5946b6
Ahmet Babacan <>
2025-08-16 02:41:35 +0300

    added search icon

---
+++ app/src/main/java/com/babacan/eterationcase/ui/icons/Searchicon.kt
@@ -0,0 +1,130 @@
+package com.babacan.eterationcase.ui.icons
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.PathFillType.Companion.EvenOdd
+import androidx.compose.ui.graphics.SolidColor
+import androidx.compose.ui.graphics.StrokeCap.Companion.Butt
+import androidx.compose.ui.graphics.StrokeJoin.Companion.Miter
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.ImageVector.Builder
+import androidx.compose.ui.graphics.vector.path
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+public val Icons.Searchicon: ImageVector
+    get() {
+        if (_searchicon != null) {
+            return _searchicon!!
+        }
+        _searchicon =
+            Builder(
+                    name = "Searchicon",
+                    defaultWidth = 17.0.dp,
+                    defaultHeight = 18.0.dp,
+                    viewportWidth = 17.0f,
+                    viewportHeight = 18.0f,
+                )
+                .apply {
+                    path(
+                        fill = SolidColor(Color(0xFF000000)),
+                        stroke = null,
+                        strokeLineWidth = 0.0f,
+                        strokeLineCap = Butt,
+                        strokeLineJoin = Miter,
+                        strokeLineMiter = 4.0f,
+                        pathFillType = EvenOdd,
+                    ) {
+                        moveTo(12.0f, 7.0f)
+                        curveTo(12.0f, 9.761f, 9.761f, 12.0f, 7.0f, 12.0f)
+                        curveTo(4.239f, 12.0f, 2.0f, 9.761f, 2.0f, 7.0f)
+                        curveTo(2.0f, 4.239f, 4.239f, 2.0f, 7.0f, 2.0f)
+                        curveTo(9.761f, 2.0f, 12.0f, 4.239f, 12.0f, 7.0f)
+                        close()
+                        moveTo(10.594f, 13.008f)
+                        curveTo(9.543f, 13.638f, 8.314f, 14.0f, 7.0f, 14.0f)
+                        curveTo(3.134f, 14.0f, 0.0f, 10.866f, 0.0f, 7.0f)
+                        curveTo(0.0f, 3.134f, 3.134f, 0.0f, 7.0f, 0.0f)
+                        curveTo(10.866f, 0.0f, 14.0f, 3.134f, 14.0f, 7.0f)
+                        curveTo(14.0f, 8.828f, 13.3f, 10.492f, 12.153f, 11.738f)
+                        lineTo(16.207f, 15.793f)
+                        curveTo(16.598f, 16.183f, 16.598f, 16.817f, 16.207f, 17.207f)
+                        curveTo(15.817f, 17.598f, 15.183f, 17.598f, 14.793f, 17.207f)
+                        lineTo(10.594f, 13.008f)
+                        close()
+                    }
+                    path(
+                        fill = SolidColor(Color(0xFF2A59FE)),
+                        stroke = null,
+                        fillAlpha = 0.3f,
+                        strokeLineWidth = 0.0f,
+                        strokeLineCap = Butt,
+                        strokeLineJoin = Miter,
+                        strokeLineMiter = 4.0f,
+                        pathFillType = EvenOdd,
+                    ) {
+                        moveTo(12.0f, 7.0f)
+                        curveTo(12.0f, 9.761f, 9.761f, 12.0f, 7.0f, 12.0f)
+                        curveTo(4.239f, 12.0f, 2.0f, 9.761f, 2.0f, 7.0f)
+                        curveTo(2.0f, 4.239f, 4.239f, 2.0f, 7.0f, 2.0f)
+                        curveTo(9.761f, 2.0f, 12.0f, 4.239f, 12.0f, 7.0f)
+                        close()
+                        moveTo(10.594f, 13.008f)
+                        curveTo(9.543f, 13.638f, 8.314f, 14.0f, 7.0f, 14.0f)
+                        curveTo(3.134f, 14.0f, 0.0f, 10.866f, 0.0f, 7.0f)
+                        curveTo(0.0f, 3.134f, 3.134f, 0.0f, 7.0f, 0.0f)
+                        curveTo(10.866f, 0.0f, 14.0f, 3.134f, 14.0f, 7.0f)
+                        curveTo(14.0f, 8.828f, 13.3f, 10.492f, 12.153f, 11.738f)
+                        lineTo(16.207f, 15.793f)
+                        curveTo(16.598f, 16.183f, 16.598f, 16.817f, 16.207f, 17.207f)
+                        curveTo(15.817f, 17.598f, 15.183f, 17.598f, 14.793f, 17.207f)
+                        lineTo(10.594f, 13.008f)
+                        close()
+                    }
+                    path(
+                        fill = SolidColor(Color(0xFFffffff)),
+                        stroke = null,
+                        fillAlpha = 0.5f,
+                        strokeLineWidth = 0.0f,
+                        strokeLineCap = Butt,
+                        strokeLineJoin = Miter,
+                        strokeLineMiter = 4.0f,
+                        pathFillType = EvenOdd,
+                    ) {
+                        moveTo(12.0f, 7.0f)
+                        curveTo(12.0f, 9.761f, 9.761f, 12.0f, 7.0f, 12.0f)
+                        curveTo(4.239f, 12.0f, 2.0f, 9.761f, 2.0f, 7.0f)
+                        curveTo(2.0f, 4.239f, 4.239f, 2.0f, 7.0f, 2.0f)
+                        curveTo(9.761f, 2.0f, 12.0f, 4.239f, 12.0f, 7.0f)
+                        close()
+                        moveTo(10.594f, 13.008f)
+                        curveTo(9.543f, 13.638f, 8.314f, 14.0f, 7.0f, 14.0f)
+                        curveTo(3.134f, 14.0f, 0.0f, 10.866f, 0.0f, 7.0f)
+                        curveTo(0.0f, 3.134f, 3.134f, 0.0f, 7.0f, 0.0f)
+                        curveTo(10.866f, 0.0f, 14.0f, 3.134f, 14.0f, 7.0f)
+                        curveTo(14.0f, 8.828f, 13.3f, 10.492f, 12.153f, 11.738f)
+                        lineTo(16.207f, 15.793f)
+                        curveTo(16.598f, 16.183f, 16.598f, 16.817f, 16.207f, 17.207f)
+                        curveTo(15.817f, 17.598f, 15.183f, 17.598f, 14.793f, 17.207f)
+                        lineTo(10.594f, 13.008f)
+                        close()
+                    }
+                }
+                .build()
+        return _searchicon!!
+    }
+
+private var _searchicon: ImageVector? = null
+
+@Preview
+@Composable
+private fun Preview() {
+    Box(modifier = Modifier.padding(12.dp)) {
+        Image(imageVector = Icons.Searchicon, contentDescription = null)
+    }
+}
